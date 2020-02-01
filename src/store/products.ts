8c2195fdb1459e21acba8cbf285f0938225dbe5f
Robert Piosik <robertpiosik@gmail.com>
2020-02-01 15:29:41 +0100

    feat: products store module

--- src/store/products.ts
+++ src/store/products.ts
@@ -1,11 +1,37 @@
 import { getterTree, mutationTree, actionTree } from 'nuxt-typed-vuex'
 
-interface State {}
+interface Product {
+  id: number
+  thumbnail: string
+  title: string
+  price: number
+}
 
-export const state = (): State => ({})
+interface State {
+  products: Array<Product> | null
+}
+
+export const state = (): State => ({
+  products: null
+})
 
 export const getters = getterTree(state, {})
 
-export const mutations = mutationTree(state, {})
+export const mutations = mutationTree(state, {
+  setProducts(state, productsData: State['products']) {
+    state.products = productsData
+  }
+})
 
-export const actions = actionTree({ state, getters, mutations }, {})
+export const actions = actionTree(
+  { state, getters, mutations },
+  {
+    async getProducts({ commit }) {
+      const productsData: Array<Product> = await fetch(
+        'http://www.mocky.io/v2/5e3588c72f00007c00793483'
+			).then(r => r.json())
+			
+			commit('setProducts', productsData);
+    }
+  }
+)
