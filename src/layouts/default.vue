91761c0d00612ae5d19a9f75b4717d937cea7e7a
Robert Piosik <robertpiosik@gmail.com>
2020-02-03 21:17:44 +0100

    feat: default layout header

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,19 +1,32 @@
 <template lang="pug">
-	main
+	div
+		header(:class="$style.header")
+			div(:class="$style.headerWrapper")
+				div(:class="$style.headerText") Nuxt.js &amp; TypeScript Example App
 		nuxt
 		footer(:class="$style.footer")
-				div(:class="$style.drinkWater") Stay hydrated 💧
+				div(:class="$style.footerReminder") Stay hydrated 💧
 </template>
 
 <style lang="scss" module>
+.header {
+  background: $primary;
+}
+.headerWrapper {
+  @include wrapper;
+}
+.headerText {
+	color: white;
+	font-weight: bold;
+	padding: 1rem 0;
+}
 .footer {
   margin: 8rem 0 6rem 0;
-	@include atSmall {
-  margin: 12rem 0 10rem 0;
-
-	}
+  @include atSmall {
+    margin: 12rem 0 10rem 0;
+  }
 }
-.drinkWater {
+.footerReminder {
   text-align: center;
 }
 </style>
