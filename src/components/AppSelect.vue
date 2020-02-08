c74c4093812901701ec442450360a72081d09048
Robert Piosik <robertpiosik@gmail.com>
2020-02-08 07:54:09 +0100

    feat: AppSelect

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+	select(v-model="value" @change="input()")
+		option(disabled value="") {{ label }}
+		option(v-for="(option, index) in options" :key="index" :value="option.value") {{ option.label }}
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit } from 'vue-property-decorator'
+
+@Component
+export default class AppSelect extends Vue {
+	@Prop({type: String, required: true}) readonly label!: String
+	@Prop({ type: Array, required: true }) readonly options!: object[]
+
+	value = ""
+
+	@Emit()
+	input() {
+		return this.value
+	}
+
+}
+</script>
