5f4cee4849e32b4b28719f5f3b68d80786a883d7
Robert Piosik <robertpiosik@gmail.com>
2020-02-08 08:12:45 +0100

    refactor: event names

---
+++ __tests__/components/AppProductsGridItem.spec.ts
@@ -0,0 +1,68 @@
+import { mount } from '@vue/test-utils'
+import AppProductsGridItem from './../../src/components/AppProductsGridItem.vue'
+
+import { priceFormatter } from './../../src/utils/transformations'
+
+const title = 'Some title'
+const price = 20.99
+const thumbnail = 'someUrl'
+
+const wrapper = mount(AppProductsGridItem, {
+  propsData: {
+    title,
+    price,
+    thumbnail
+  }
+})
+
+describe('Component: ProductsGrid', () => {
+  describe('GENERAL', () => {
+    it('should match a snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+    it('should be a Vue instance', () => {
+      expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+  })
+  describe('PROPS', () => {
+    describe('title', () => {
+      it('should receive', () => {
+        expect(wrapper.props('title')).toBe(title)
+      })
+      it('should render', () => {
+        expect(wrapper.text()).toContain(title)
+      })
+    })
+    describe('price', () => {
+      it('should receive', () => {
+        expect(wrapper.props('price')).toBe(price)
+      })
+      it('should NOT render passed unformatted number', () => {
+        expect(wrapper.text()).not.toContain(price)
+      })
+      it('should render formatted price', () => {
+        const formattedPrice = priceFormatter(price)
+        expect(wrapper.text()).toContain(
+          formattedPrice.value + formattedPrice.penny
+        )
+      })
+    })
+    describe('thumbnail', () => {
+      it('should receive', () => {
+        expect(wrapper.props('thumbnail')).toBe(thumbnail)
+      })
+      it('should output html containing passed value', () => {
+        expect(wrapper.html()).toContain(thumbnail)
+      })
+    })
+  })
+  describe('EVENTS', () => {
+    describe('addToCart', () => {
+      it('should emit by clicking on a button[addToCart]', () => {
+        const button = wrapper.find('button[addToCart]')
+        button.trigger('click')
+        expect(wrapper.emitted('add-to-cart')).toBeTruthy()
+      })
+    })
+  })
+})
