e1d9f2a142686d233ce9c1951ff4ce8c0904ca77
Robert Piosik <robertpiosik@gmail.com>
2020-02-09 20:42:05 +0100

    test: update transformations

--- __tests__/utils/transformations.spec.ts
+++ __tests__/utils/transformations.spec.ts
@@ -2,11 +2,20 @@ import { priceFormatter } from '../../src/utils/transformations'
 
 describe('Utils: transformations', () => {
   describe('priceFormatter', () => {
-    it('should correctly transform 10 (number)', () => {
+    it('should correctly transform 10', () => {
       expect(priceFormatter(10)).toMatchObject({ value: '10', penny: ',00' })
     })
-    it('should transform 10.99 (number)', () => {
+    it('should correctly transform 10.99', () => {
       expect(priceFormatter(10.99)).toMatchObject({ value: '10', penny: ',99' })
+    })
+    it('should correctly transform 10.99', () => {
+      expect(priceFormatter(10.9)).toMatchObject({ value: '10', penny: ',90' })
+    })
+    it('should correctly transform 10.00001', () => {
+      expect(priceFormatter(10.00001)).toMatchObject({
+        value: '10',
+        penny: ',00'
+      })
     })
   })
 })
